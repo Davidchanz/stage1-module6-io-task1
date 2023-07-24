909b8049a1c196ff6429290ea8184d0beed8697e
Davidchanz <tovliskatsi@gmail.com>
2023-07-24 16:18:37 +0400

    Java IO Task 1 Quality fixed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -12,56 +12,48 @@ public class FileReader {
         Profile profile = new Profile();
         try(FileInputStream fileReader = new FileInputStream(file)) {
             int c;
-            StringBuilder fileText = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
             while ((c = fileReader.read()) != -1){
-                fileText.append(Character.toString(c));
+                stringBuilder.append(Character.toString(c));
             }
-            var lines= fileText.toString().split("\n");
+            var lines= stringBuilder.toString().split("\n");
             for(var line: lines) {
                 var lineEntry = parseLine(line);
                 setProfileField(profile, lineEntry);
             }
         } catch (IOException e) {
-            System.err.println("File IO Error!");
             throw new RuntimeException(e);
         }
         return profile;
     }
 
-    private void setProfileField(Profile profile, Map.Entry<String, Object> lineEntry) {
-        try {
-            var field = profile.getClass().getDeclaredField(lineEntry.getKey().toLowerCase());
-            field.setAccessible(true);
-            field.set(profile, lineEntry.getValue());
-            field.setAccessible(false);
-        } catch (NoSuchFieldException e) {
-            System.err.println("Error! No field " + lineEntry.getKey());
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            System.err.println("Error! Can't set field " + lineEntry.getKey());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private Map.Entry<String, Object> parseLine(String line) throws IllegalArgumentException{
-        var entry = line.split(": ");
-        String key = entry[0];
-        String rawValue = entry[1];
-        Object value = rawValue;
-        switch (key){
-            case "Name":
-            case "Email": {
+    private void setProfileField(Profile profile, Map.Entry<String, String> lineEntry) {
+        switch (lineEntry.getKey()){
+            case "Name" :{
+                profile.setName(lineEntry.getValue());
                 break;
             }
             case "Age":{
-                value = Integer.parseInt(rawValue);
+                profile.setAge(Integer.parseInt(lineEntry.getValue()));
+                break;
+            }
+            case "Email":{
+                profile.setEmail(lineEntry.getValue());
                 break;
             }
             case "Phone":{
-                value = Long.parseLong(rawValue);
+                profile.setPhone(Long.parseLong(lineEntry.getValue()));
                 break;
             }
+            default:{
+            }
         }
+    }
+
+    private Map.Entry<String, String> parseLine(String line){
+        var entry = line.split(": ");
+        String key = entry[0];
+        String value = entry[1];
         return Map.entry(key, value);
     }
 }
